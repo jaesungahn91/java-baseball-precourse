a32f6990628b21cf3b300290840263de32297dd0
jaesungahn91 <jaesungahn91@gmail.com>
2022-04-18 12:08:39 +0900

    test: 테스트 명칭 및 코드 수정

--- src/test/java/baseball/domain/NumberTest.java
+++ src/test/java/baseball/domain/NumberTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class NumberTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {0, 10})
+    @ValueSource(ints = {0, 10, -1})
     void 예외테스트_범위_이외의_숫자_생성(final int value) {
         assertThatThrownBy(() -> new Number(value))
                 .isInstanceOf(IllegalArgumentException.class);
--- src/test/java/baseball/domain/PlayerNumbersTest.java
+++ src/test/java/baseball/domain/PlayerNumbersTest.java
@@ -11,12 +11,14 @@ class PlayerNumbersTest {
     @ParameterizedTest
     @ValueSource(strings = {"123", "671", "111", "789"})
     void 플레이어_3자리의_숫자_생성(final String numbers) {
-        PlayerNumbers playerNumbers = new PlayerNumbers();
-        playerNumbers.setNumbersByStringNumbers(numbers.split(""));
+        String[] numberArr = numbers.split("");
 
-        assertThat(playerNumbers.getNumbers()).containsExactly(new Number(numbers.split("")[0]),
-                new Number(numbers.split("")[1]),
-                new Number(numbers.split("")[2]));
+        PlayerNumbers playerNumbers = new PlayerNumbers();
+        playerNumbers.setNumbersByStringNumbers(numberArr);
+
+        assertThat(playerNumbers.getNumbers()).containsExactly(new Number(numberArr[0]),
+                new Number(numberArr[1]),
+                new Number(numberArr[2]));
     }
 
 }
